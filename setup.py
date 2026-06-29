d44247d6f788c19bf9d56af0c9836d4ed79cab43
rehaan <rehanxalam786@gmail.com>
2026-06-29 21:30:08 +0530

    Add setup.py for terminal-hackbar package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terminal-hackbar",
+    version="2.0.0",
+    author="Syed Rehan",
+    author_email="rehuonly@aol.com",
+    description="Terminal HackBar - Advanced Security Testing Toolkit",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rehuux/vulnterm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.28.0",
+        "colorama>=0.4.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hackbar=hackbar.__main__:main",
+        ],
+    },
+)
